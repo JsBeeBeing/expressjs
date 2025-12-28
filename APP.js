2e6db7526c821475aa7d958aa0d1e3e18b5f92d3
José <js.espinozza@gmail.com>
2025-12-28 14:52:30 -0600

    enviornment settings

--- APP.js
+++ APP.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3001
 
 app.get('/',(req,res)=>{
     res.send(
@@ -14,5 +14,3 @@ app.listen(PORT,(req,res)=>{
     console.log(`Funcionando en puerto ${PORT}`);
     
 })
-
-const a ='Prueba'
