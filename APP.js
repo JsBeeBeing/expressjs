81ca55ef3ee8fc12ebfdd6836f597c61aa4ed39d
José <js.espinozza@gmail.com>
2026-01-04 18:43:09 -0600

    Uso de metodos POST y GET para agregar usuario

--- APP.js
+++ APP.js
@@ -1,16 +1,49 @@
+require('dotenv').config()
+const { error, log } = require('console')
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001
+const fs = require('fs')
+const path = require('path')
 
+console.log(process.env.PORT);
+
+app.use(express.json())
 app.get('/',(req,res)=>{
     res.send(
-        `<h1>Hello World</h1>
-        <p>Funcionando en ${PORT}</p>
+        `<h1>Ayer estaba con Lebron tambien con Di Caprio</h1>
+        <h3>Di monaco</h3>
+        <p>Funcionando en puerto ${PORT}</p>
         `
     )
 })
 
-app.listen(PORT,(req,res)=>{
-    console.log(`Funcionando en puerto ${PORT}`);
+app.get('/user',(req,res)=>{
+    fs.readFile('usuarios.json', 'utf-8',(err,data)=>{
+        if(err)return res.status(500).send('Error');
+        const usuarios = JSON.parse(data);
+        
+        return res.json(usuarios)
+        
+    })
+})
+
+app.post('/user',(req,res)=>{
+    const newUser = req.body
+    fs.readFile('usuarios.json','utf-8',(err,data)=>{
+        if (err) return res.status(500).send(`Error ${err}`)
+        const usuarios = JSON.parse(data);
+        usuarios.push(newUser) /* Agrega al nuevo usuario dentro del texto, despues se reescribe este */
+        fs.writeFile('usuarios.json',JSON.stringify(usuarios, null, 2),(err)=>{
+            if(err) return res.status(500).send('Fallo el servidor')
+                res.status(201).json({message: 'usuario creado',
+             usuario: newUser})
+       })  
+    })
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Funcionando http://localhost:${PORT}/`);
     
 })
